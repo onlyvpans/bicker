c3c48547ebd7827e05eddfa28da7e90c662b6f8a
viv <vivpan95@gmail.com>
2025-10-01 10:28:21 +1000

    feat: set up auth routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { AuthenticationPage } from "../auth/AuthenticationPage";
+import { ClerkProviderWithRoutes } from "./auth/ClerkProviderWithRoutes";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ClerkProviderWithRoutes>
+      <Routes>
+        <Route path="/sign-in/*" element={<AuthenticationPage />}></Route>
+        <Route path="/sign-up" element={<AuthenticationPage />}></Route>
+        <Route element={<Layout />}>
+          {/* put everything that sits in layout here */}
+        </Route>
+      </Routes>
+    </ClerkProviderWithRoutes>
+  );
 }
 
-export default App
+export default App;
